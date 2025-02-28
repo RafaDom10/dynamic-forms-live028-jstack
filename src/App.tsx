7e870fc44845d370888e316b5be3c0536e6afb8b
Rafael Domingues <r-domingues@outlook.com>
2025-02-28 14:48:23 -0300

    add form link

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import { ThemeProvider } from "./context/ThemeProvider"
+import { PlusCircleIcon, Trash2Icon } from "lucide-react"
+import { Button } from "./components/Button"
+import { Input } from "./components/Input"
+import { Label } from "./components/Label"
 
 export function App() {
   return (
-    <ThemeProvider defaultTheme="dark">
-      <div>
-        <h1>App</h1>
+    <div className="grid place-items-center min-h-screen">
+      <div className="w-full max-w-2xl">
+        <h1 className="text-2xl font-semibold tracking-tight">Links</h1>
+        <form className="mt-10 flex flex-col gap-4">
+          <div className="flex gap-4">
+            <div className="flex-1 space-y-2">
+              <Label htmlFor="title">Título</Label>
+              <Input id="title" />
+            </div>
+            <div className="flex-1 flex gap-4 items-end">
+              <div className="flex-1 space-y-2">
+                <Label htmlFor="url">URL</Label>
+                <Input id="url" />
+              </div>
+
+              <Button variant="destructive">
+                <Trash2Icon className="size-4"/>
+              </Button>
+            </div>
+          </div>
+
+          <Button className="w-full border-dashed mt-6" variant="outline">
+            <PlusCircleIcon className="size-4 mr-1" />
+            Adicionar novo link
+          </Button>
+        </form>
       </div>
-    </ThemeProvider>
+    </div>
   )
 }
